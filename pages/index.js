2cbbcd5e24607926b8edb38eb8947d5eca8229a4
Antônio Terhorst <84198804+AntonioTerhorstDelgado@users.noreply.github.com>
2026-06-22 19:56:46 +0000

    substitui componentes mui por inputs html nativos e apliquei css module

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,25 @@
-import cadastroPage from "../pages/cadastro/index";
+import { Container, Button, Box } from "@mui/material";
+import Link from "next/link";
+import styles from "../styles/index.module.css";
 
-function Home() {
-  return cadastroPage();
+export default function Home() {
+  return (
+    <Container className="container" sx={{ textAlign: "center", mt: 10 }}>
+      <h1 className="h1">Bem-vindo ao EstoquePro</h1>
+      <p>Gestão eficiente e simples para seus produtos.</p>
+
+      <Box sx={{ mt: 5, display: "flex", gap: 2, justifyContent: "center" }}>
+        <Link href="/login" passHref>
+          <Button variant="contained" size="large">
+            Entrar no Sistema
+          </Button>
+        </Link>
+        <Link href="/cadastro" passHref>
+          <Button variant="outlined" size="large">
+            Criar Conta
+          </Button>
+        </Link>
+      </Box>
+    </Container>
+  );
 }
-
-export default Home;
